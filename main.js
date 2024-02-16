6cd2e04829530a766464ea0eaf6713ed2cfaaccc
Darwin <darwin78913@gmail.com>
2024-02-15 23:30:17 -0500

    wip

--- main.js
+++ main.js
@@ -1,58 +1,18 @@
 
-class Snow {
 
-    delta = 8;
-
-    constructor(x, y, length) {
-        this.x = x;
-        this.y = y;
-        this.length = length;
-    }
-
-    moveDown() {
-        this.x -= this.delta;
-        this.y += this.delta;
-
-        if (this.x <= 0 || this.y >= height) {
-            if (int(random(0, 1) < 0.5)) {
-                // start on top
-                this.y = 0;
-                this.x = int(random(0, width));
-            } else {
-                // start on side
-                this.x = width;
-                this.y = int(random(0, height));
-            }
+$(document).ready(() => {
+    $("#starting-input").on("input", function() {
+        let inputStr = $(this).val();
+        
+        if (inputStr.toLowerCase() == "bingus") {
+            $(this).prop('disabled', true);
+            setTimeout(() => {start()}, 1000);
         }
-    }
-}
-
-var snowList = [];
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    for (let i = 0; i < 100; i++) {
-        snowList.push(new Snow(int(random(0, width)), int(random(0, height)), int(random(2, 8))));
-    }
-}
-
-function windowResized() {
-    const css = getComputedStyle(canvas.parentElement),
-          marginWidth = round(float(css.marginLeft) + float(css.marginRight)),
-          marginHeight = round(float(css.marginTop) + float(css.marginBottom)),
-          w = windowWidth - marginWidth, h = windowHeight - marginHeight;
-  
-    resizeCanvas(w, h, true);
-  }
-  
-function draw() {
-    background(0);
-
-    stroke(color(255, 255, 255));
-    strokeWeight(3);
-    snowList.forEach( (e) => {
-        line(e.x, e.y, e.x - int(e.length/2), e.y + e.length);
-
-        e.moveDown();
     });
+});
+
+function start() {
+    let fadeTime = 1500;
+    $('#start-intro').fadeOut(fadeTime);
+    setTimeout(() => {currState = STATE_RAIN}, fadeTime * 1/10);
 }
---
+++ sketch.js
@@ -0,0 +1,77 @@
+let rainSound;
+
+const STATE_START = 0;
+const STATE_RAIN = 1;
+let currState = STATE_START;
+
+class Snow {
+
+    delta = 8;
+
+    constructor(x, y, length) {
+        this.x = x;
+        this.y = y;
+        this.length = length;
+    }
+
+    moveDown() {
+        this.x -= this.delta;
+        this.y += this.delta;
+
+        if (this.x <= 0 || this.y >= height) {
+            if (int(random(0, 1) < 0.5)) {
+                // start on top
+                this.y = 0;
+                this.x = int(random(0, width));
+            } else {
+                // start on side
+                this.x = width;
+                this.y = int(random(0, height));
+            }
+        }
+    }
+}
+
+var snowList = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for (let i = 0; i < 100; i++) {
+        snowList.push(new Snow(int(random(0, width)), int(random(0, height)), int(random(2, 8))));
+    }
+
+    rainSound = loadSound("resources/rain.mp3");
+    rainSound.setVolume(0.003);
+    drawSnow();
+}
+  
+function draw() {
+    if (currState == STATE_RAIN) {
+        if (rainSound.isLoaded() && !rainSound.isPlaying()) {
+            rainSound.play();
+            rainSound.setLoop(true);
+        }
+        drawSnow();
+    }
+}
+
+function drawSnow() {
+    background(0);
+
+    stroke(color(255, 255, 255));
+    strokeWeight(3);
+    snowList.forEach( (e) => {
+        line(e.x, e.y, e.x - int(e.length/2), e.y + e.length);
+
+        e.moveDown();
+    });
+}
+
+function windowResized() {
+    const css = getComputedStyle(canvas.parentElement),
+          marginWidth = round(float(css.marginLeft) + float(css.marginRight)),
+          marginHeight = round(float(css.marginTop) + float(css.marginBottom)),
+          w = windowWidth - marginWidth, h = windowHeight - marginHeight;
+  
+    resizeCanvas(w, h, true);
+}
